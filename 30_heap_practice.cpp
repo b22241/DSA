4590cafa4fa7d440e0a851a485ff6e995175d999
b22241 <freesoul6800@gmail.com>
2025-09-03 22:17:47 +0530

    Updated heap file

--- 30_heap_practice.cpp
+++ 30_heap_practice.cpp
@@ -1,3 +1,21 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
+
+
+int main(){
+    priority_queue<int> pq;
+    pq.push(10);
+    pq.push(30);
+    pq.push(20);
+    pq.push(5);
+
+    // Extract elements (largest first)
+    cout << "Max Heap: ";
+    while (!pq.empty()) {
+        cout << pq.top() << " "; // access largest
+        pq.pop();
+    }
+    cout << endl;
+}
