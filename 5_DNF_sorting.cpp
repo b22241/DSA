8c0af2ade7e262c0ac16e5c00eebcd20856cafe4
Suman Deep <Suman@VTPPL.COM>
2025-09-05 13:12:06 +0530

    let's go

--- 5_DNF_sorting.cpp
+++ 5_DNF_sorting.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <algorithm> // For std::swap and std::reverse
+#include <map>
+#include <set>
+#include <vector>
 using namespace std;
 
 void print_array(int arr[], int n) {
@@ -9,6 +12,7 @@ void print_array(int arr[], int n) {
     cout << endl;
 }
 
+//brute force appraoch to sort numbers(colors) in arr
 void sorting(int arr[],int n){
     int zero=0;
     int one=0;
@@ -29,10 +33,28 @@ void sorting(int arr[],int n){
         arr[k++]=2;
     }
 }
+//hashmap appraoch  //Time: O(nlogn) Space: O(1)
+void sort(int arr[],int n){
+    map<int,int>m;
+    for(int i=0;i<n;i++){
+        m[arr[i]]++;
+    }
+    for(auto x:m){
+        for(int i=0;i<x.second;i++){
+            cout<<x.first<<" ";
+        }
+    }
+}
+//multiset appraoch
+void sortArray(int arr[], int n) {
+    multiset<int> s(arr, arr+n); // O(n log n)
+    for (int x : s) cout << x << " ";
+}
 
+//Dutch National Flag (DNF) algorithm
+//Time: O(n) Space: O(1)
 void DNF_sorting(int arr[], int n) {
     int low = 0, mid = 0, high = n - 1;
-
     while (mid <= high) {
         if (arr[mid] == 0) {
             swap(arr[low], arr[mid]);
@@ -46,6 +68,29 @@ void DNF_sorting(int arr[], int n) {
             swap(arr[mid], arr[high]);
             high--; 
         }
+    }
+}
+
+//O(n) and O(n) space complexity
+void merge_2_sorted_arr(vector<int>& arr1, int n1, vector<int>& arr2, int n2) {
+    int i = n1 - n2 - 1;
+    int j = n2 - 1;
+    int idx = n1 - 1;
+    while (i >= 0 && j >= 0) {
+        if (arr1[i] < arr2[j]) {
+            arr1[idx--] = arr2[j--];
+        } else {
+            arr1[idx--] = arr1[i--];
+        }
+    }
+    while (i >= 0) {
+        arr1[idx--] = arr1[i--];
+    }
+    while (j >= 0) {
+        arr1[idx--] = arr2[j--];
+    }
+    for(int i=0;i<n1;i++){
+        cout<<arr1[i]<<" ";
     }
 }
 
@@ -79,11 +124,54 @@ void next_permutation(int arr[], int n) {
 }
 
 
+
+void next(int arr[], int n) {
+    int pivot = -1;
+
+    // find pivot
+    for (int i = n - 1; i > 0; i--) {
+        if (arr[i - 1] < arr[i]) {   // correct condition
+            pivot = i - 1;
+            break;
+        }
+    }
+
+    if (pivot == -1) {
+        // if no pivot found, reverse whole array (last permutation)
+        reverse(arr, arr + n);
+    } else {
+        // find element just greater than pivot from right
+        for (int i = n - 1; i > pivot; i--) {
+            if (arr[i] > arr[pivot]) {
+                swap(arr[i], arr[pivot]);
+                break;
+            }
+        }
+        // reverse suffix
+        reverse(arr + pivot + 1, arr + n);
+    }
+
+    // print result
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
 int main(){
     int arr[]={1,2,3,6,5,4};
     int n=sizeof(arr) / sizeof(arr[0]);
-    next_permutation(arr,n);
-    print_array(arr,n);
+    vector<int>arr1={1,2,3,0,0,0};
+    int n1=arr1.size();
+    vector<int>arr2={2,5,6};
+    int n2=arr2.size();
+    // print_array(arr,n);
+    // sort(arr,n);
+    // merge_2_sorted_arr(arr1,n1,arr2,n2);
+    // next_permutation(arr,n);
+    int arr3[]={1,2,5,4,3};
+    int n3=sizeof(arr3) / sizeof(arr3[0]);
+    next(arr3,n3);
 
 
 }
