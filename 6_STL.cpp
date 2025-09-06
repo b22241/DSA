1a1424342143d960402921ecbf643d6410f7f105
Suman Deep <Suman@VTPPL.COM>
2025-09-06 11:28:36 +0530

    STL completed

--- 6_STL.cpp
+++ 6_STL.cpp
@@ -10,18 +10,21 @@
 #include <set>
 using namespace std;
 
-///VECTORS
+
 // int main(){
+///VECTORS
 //     vector <int> vec={4,2,3,55,323,2,4,5,26,74,34,53};
+//     vector <int> vec={3,10};  {10,10,10}
+//     vector <int> vec2{vec};  {10,10,10}  //copy of vector
 //     for(int val:vec){
 //         cout<<val<<" ";
 //     }
 //     cout<<endl;
 //     //vec.push_back(100);
 //     //vec.pop_back();
-//     //vec.erase(vec.begin()+3);
+//     //vec.erase(vec.begin()+3);         //O(n)
 //     //vec.erase(vec.begin(),vec.end()-1);
-//     //vec.insert(vec.begin()+1,10);
+//     //vec.insert(vec.begin()+1,10);      //O(n)
 //    // vec.clear();
 //    //cout<<vec.empty();   prints 0 or 1 
 //   
@@ -40,11 +43,12 @@ using namespace std;
 
 
 //LIST
+// random accces is not possible in list unlike vector,arr,deque
 //DEQUE
 // int main(){
 //     list<int> l;
 //     deque<int> d={2,436,3,6,785,34,10};
-//    l.push_back(2);
+//     l.push_back(2);
 //     l.push_back(9);
 //     l.push_back(-1);
 //     l.push_back(36);
@@ -67,10 +71,9 @@ using namespace std;
 //     cout<<p1.first<<endl;
 //     cout<<p1.second.first;
 // }
+
  
- 
-//LIFO
-//stack
+//stack  //LIFO  O(1)
 // int main(){
 //     stack<int> s ;
 //     s.push(1);
@@ -79,35 +82,42 @@ using namespace std;
 //     s.push(12);
 //     s.push(9);
 //     s.pop();
+//     stack<int>s2;
+//     s2.swap(s);  //pushes all values of stack s to another stack s2
+//     cout<<s.top();
 //     cout<<s.size()<<endl;
 //     cout<<s.empty()<<endl;
 //     cout<<s.top()<<endl;
 // }
 
 
-//FIFO
+// Queue   //FIFO  allfunctions are same as stack
 // int main(){
 //     queue<int> q;
 //     q.push(1);
-//     //     s.push(4);
-//     //     s.push(432);
-//     //     s.push(12);
-//     //     s.push(9);
+//     //     q.push(4);
+//     //     q.push(432);
+//     //     q.push(12);
+//     //     q.push(9);
 //     cout<<q.front(); 
 // }
 
-//priority queue
+
+//priority queue   data is stored in maxheap or minheap format. it's stored in storted order  complexity-O(logn)
 // int main(){
 //     priority_queue<int>q;
 //     q.push(2);
 //     q.push(4);
 //     q.push(21);
 //     q.push(-1);
+    //    q.pop();
+// q.size();
+// q.empty();
 //     cout<<q.top();
 // }
 
 
-//map
+//map  sort by key values
 //time complexity O(logn);
 // int main(){
 //     map<string,int>m;
@@ -116,21 +126,27 @@ using namespace std;
 //     m["headphone"]=20;
 //     m["tab"]=30;
 //     m.insert({"camera",23});
+    // m.emplace("camera1",23); 
 //     m.erase("tv");
 //     cout<<m["tab"]<<endl;
 //     for(auto p:m){
 //         cout<<p.first<<" "<<p.second<<endl;
 //     }
+// if(m.find("camera")!=m.end()){
+    // cout<<"camera found";
+// }
 // }
 
-//multimap ===allows duplicate values of a attribute
+
+//multimap ===allows duplicate values of a attribute,everything is same as map, b
 // int main(){
 //     multimap<string,int>m;
 //     m.emplace("tv",23);
 //     m.emplace("tv",24);
 //     m.emplace("tv",22);
 //     m.emplace("tv",4345);
-//     //m.erase("tv");
+//     m.erase("tv");   sari values khali ho jaegi
+//       m.erase(m.find("tv"));   phli value hi clear hogi
 //     for(auto p:m){
 //                 cout<<p.first<<" "<<p.second<<endl;
 //             }
@@ -140,27 +156,27 @@ using namespace std;
 //unordered_map , arranges data in random order
 //time complexity O(1);
 //search time for ordered map is O(logn) and for unorderd map is O(1),every operation happens in constast time in unordered map
-int main(){
-    unordered_map<string,int>m;
-    m.emplace("tv",23);
-    m.emplace("tv",24);
-    m.emplace("tab",24);
-    m.emplace("phone",22);
-    m.emplace("moble",4345);
-    //m.erase("tv");
-    m.insert({"ram",20});
-    m["shyam"]=30;
-    m["shiv"]=40;
-    // cout<<m["ram"];
-    // cout<<m.at("ram");
-    for(auto p:m){
-        cout<<p.first<<"-"<<p.second<<endl;
-    }
-}
+// int main(){
+//     unordered_map<string,int>m;
+//     m.emplace("tv",23);
+//     m.emplace("tv",24);
+//     m.emplace("tab",24);
+//     m.emplace("phone",22);
+//     m.emplace("moble",4345);
+//     //m.erase("tv");
+//     m.insert({"ram",20});
+//     m["shyam"]=30;
+    // m["shiv"]=40;
+//     // cout<<m["ram"];
+//     // cout<<m.at("ram");
+//     for(auto p:m){
+//         cout<<p.first<<"-"<<p.second<<endl;
+//     }
+// }
 
 
 //set ==>stores only uniques values in sorted order
-//unordered_set ==>stores only uniques values in random order in sorted order
+//unordered_set ==>stores only uniques values in random order
 //multiset==> stores duplicate values as well
 //time complexity O(logn)
 // int main(){
@@ -194,9 +210,9 @@ int main(){
 // }
 
 
-//next_permutation
-//previouse_permutation
-//swap
-//min
-//max
+//next_permutation  next_permutation(vec.begin(),vec.end());
+//previouse_permutation    previous_permutation(vec.begin(),vec.end());
+//swap   swap(a,b);
+//min   *min_element(vec.begin(),vec.end())
+//max   *max_element(vec.begin(),vec.end())
 //builtin_popcount
